e68562b4a8060fe5f9d113872f0c1d9a7dbd05c7
SINETHDINUWARA <sinethdinuwara19@gmail.com>
2024-05-13 14:57:06 +0530

    complete tasify to-do app

--- app/src/main/java/com/example/thenotesapp/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/thenotesapp/adapter/NoteAdapter.kt
@@ -1,5 +1,10 @@
 package com.example.thenotesapp.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notesroompractice.databinding.NoteLayoutBinding
@@ -7,16 +12,44 @@ import com.example.thenotesapp.model.Note
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
-    class NoteViewHolder(val itemBinding: NoteLayoutBinding): RecyclerView.ViewHolder(itemBinding.root)
+    class NoteViewHolder(val itemBinding: NoteLayoutBinding) : RecyclerView.ViewHolder(itemBinding.root)
 
-    private val differcallback = object : DiffUtil.ItemCallback<Note>(){
+    private val differCallback = object : DiffUtil.ItemCallback<Note>() {
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
-            TODO("Not yet implemented")
+            return oldItem == newItem
         }
-
     }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    fun submitList(list: List<Note>) {
+        differ.submitList(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val itemBinding = NoteLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itemBinding.noteTitle.text = currentNote.noteTitle
+        holder.itemBinding.noteDesc.text = currentNote.noteDesc
+
+        holder.itemView.setOnClickListener { view ->
+            val direction = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(currentNote)
+            view.findNavController().navigate(direction as NavDirections)
+        }
+    }
+
 }
+
